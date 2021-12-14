a933db0c3716dacf15c2429d6892d42637fbb4bd
Kush Bajpai <35020929+bajpaik@users.noreply.github.com>
2021-12-14 14:27:55 +0530

    Update Small_Hacks.js

--- Small_Hacks.js
+++ Small_Hacks.js
@@ -21,3 +21,14 @@ console.log(str)
 function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/*****************Generate GUID***********************************************/
+function guid() {
+	  function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		  .toString(16)
+		  .substring(1);
+	  }
+	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		s4() + '-' + s4() + s4() + s4();
+	}
